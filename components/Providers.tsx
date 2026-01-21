44ad9112665c4052577aefde9361b05925ba6ddf
Mamadou Elimane Wane <votre.email@exemple.com>
2026-01-21 22:20:04 +0100

    fix: add FavoriteProvider to global providers

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { CartProvider } from '@/context/CartContext';
+import { FavoriteProvider } from '@/context/FavoriteContext';
 import { Toaster } from 'react-hot-toast';
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <CartProvider>
-            {children}
-            <Toaster position="bottom-right" />
+            <FavoriteProvider>
+                {children}
+                <Toaster position="bottom-right" />
+            </FavoriteProvider>
         </CartProvider>
     );
 }
